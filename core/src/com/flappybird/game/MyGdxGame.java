90468af653da420e37b1dd8b267a75a6bda61be3
Vendoros <Good0908>
2019-10-17 18:04:52 +0800

    0.0.2 create Backgorund (render/upDate/)

--- core/src/com/flappybird/game/MyGdxGame.java
+++ core/src/com/flappybird/game/MyGdxGame.java
@@ -7,29 +7,35 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	SpriteBatch batch; //область для отрисовки
+		Backgorund bg;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		bg = new Backgorund();
 	}
+	//загружает в память все необходимые элементы
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		upDate();
+		Gdx.gl.glClearColor(1, 1, 1, 1);//указываем цвет для заливки отрисованной области
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//заливаем отрисованную область
+		batch.begin();//начало отрисовки
+		bg.render(batch);
+		batch.end();//заканчивается отрисовка
+	}
+	//вызывается 60р в с. отрисовывает все
+
+	public void upDate(){
+		bg.upDate();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
-
+	//очищяет ресурсы.
 
 }
